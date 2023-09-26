3ae4476de939aaae6663006e8aaa41eb018a9c5b
Bruno <bruno@pop-os.localdomain>
2023-09-26 20:43:58 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import FormTarefa from "./componentes/FormTarefa";
+import ListaDeTarefas from "./componentes/ListaDeTarefas";
+import axios from "axios";
+
+const URL_BACK = 'http://localhost:5000/tarefas';
 
 function App() {
+  const [tarefas, setTarefas] = useState([]);
+
+  const carregarTarefas = () => {
+    axios.get(URL_BACK).then(({data}) => {
+      setTarefas(data);
+    });
+  };
+
+  useEffect(() => {
+    carregarTarefas();
+  }, []);
+
+  const adicionarTarefa = (form) => {
+    return axios.post(URL_BACK, form).then(() => {
+      carregarTarefas();
+    });
+  };
+
+  const excluirTarefa = (tarefa) => {
+    return axios.delete(URL_BACK + '/' + tarefa._id).then(() => {
+      carregarTarefas();
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FormTarefa onAdicionar={adicionarTarefa}/>
+      <ListaDeTarefas tarefas={tarefas} onExcluir={excluirTarefa}/>
+    </>
   );
 }
 
